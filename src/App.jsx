e5397240aa125d4febd3e3c4719126e43d3e9ffb
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-08-31 13:38:59 -0600

    Base

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ function App() {
   return (
     <>
       <div>
-
+        <div>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+        
+        <div>
+          <button>Prev</button>
+          <button>Next</button>
+        </div>
       </div>        
     </>
   )
