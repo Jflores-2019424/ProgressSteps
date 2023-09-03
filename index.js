a0147d37897dc5b51833bf39915c8bd8fd907308
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-09-03 16:12:58 -0600

    Progress steps

---
+++ index.js
@@ -0,0 +1,49 @@
+const number = document.querySelectorAll('.number')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const line = document.getElementById('line')
+
+let actualActive =  1;
+
+function update() {
+    number.forEach((number, idx) =>{
+        const actives = document.querySelectorAll(".active")
+
+        if(idx < actualActive) {
+            number.classList.add("active")
+        }else{
+            number.classList.remove("active")
+        }
+
+        line.style.width = (actives.length - 1) / (number.length  - 1) * 100 +"%"
+
+        if(actualActive === 1){
+            prev.disabled == true
+        }else if(actualActive === number.length){
+            next.disabled == true
+        }else {
+            prev.disabled == false
+            next.disabled == false
+        }
+    })
+}
+
+prev.addEventListener("click", () =>{
+    actualActive--
+
+    if(actualActive < 1){
+        actualActive = 1
+    }
+
+    update()
+})
+
+next.addEventListener("click", () =>{
+    actualActive++
+
+    if(actualActive > number.length){
+        actualActive = number.length
+    }
+
+    update()
+})
